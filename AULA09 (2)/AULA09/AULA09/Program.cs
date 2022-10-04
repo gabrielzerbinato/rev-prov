7f1bb2ca0211774ddd6d8e4632ddccb6476a42ff
gabrielzerbinato <111398927+gabrielzerbinato@users.noreply.github.com>
2022-10-04 17:08:55 -0300

    Add files via upload

---
+++ AULA09 (2)/AULA09/AULA09/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AULA09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double imposto = IRRF(2000);
+            Console.WriteLine(imposto);
+            Console.ReadLine();
+        }
+
+        static Double IRRF(Double salario)
+        {
+            Double result = 0;
+            if (salario <= 1903.80)
+            {
+                result = 0;
+            }
+            else
+            if (salario>=1903.99 && salario<=2826.65)
+            {
+                result = salario * (7.5 / 100) - 142.80;
+            }
+            else if (salario >= 2826.66 && salario <= 3751.05)
+            {
+                result = salario * (15.0 / 100) - 354.80;
+            }
+            else if (salario >= 3751.06  && salario <=   4664.68)
+            {
+                result = salario * (22.5 / 100) - 636.13;
+            }
+            else
+            {
+                result = salario * (27.5 / 100) - 869.36;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AULA09 (2)/AULA09/Exerc02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double[] lista = { 100, 150, 230, 470 };
+            Double[] nova = aumentarLista(lista, 10);
+            imprimir(lista);
+            imprimir(nova);
+            Console.WriteLine(somaLista(nova));
+            Console.ReadLine();
+        }
+
+        static Double somaLista(double[] lista)
+        {
+            Double result = 0;
+            for (int i = 0; i < lista.Length; i++)
+            {
+                result = result + lista[i];
+            }
+
+            return result;
+        }
+        static void imprimir(Double[] lista)
+        {
+            for (int i = 0; i < lista.Length; i++)
+            {
+                Console.Write(lista[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static Double[] aumentarLista(Double[] lista, Double aumento)
+        {
+            Double[] novaLista = new double[lista.Length];
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                novaLista[i] = lista[i] + lista[i]*aumento/100;
+            }
+           return novaLista;
+
+        }
+    }
+}
